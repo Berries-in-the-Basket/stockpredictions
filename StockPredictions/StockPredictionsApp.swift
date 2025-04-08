a2f8bda1f9347597e949f4ca8a617bbddfdd70e4
Mariusz <mariusz@berriesinthebasket.com>
2025-04-08 16:00:18 +0200

    Initial Commit

---
+++ StockPredictions/StockPredictionsApp.swift
@@ -0,0 +1,20 @@
+//
+//  StockPredictionsApp.swift
+//  StockPredictions
+//
+//  Created by Mariusz Smoliński on 08.04.25.
+//
+
+import SwiftUI
+
+@main
+struct StockPredictionsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
